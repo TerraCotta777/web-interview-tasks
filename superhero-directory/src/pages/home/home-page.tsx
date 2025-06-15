d1fadf1a24eeb1194c7ecb330d871c37fcc9dbea
terracotta <begaiym.akylova@gmail.com>
2025-06-15 20:20:09 +0600

    feat: add results on home page

--- superhero-directory/src/pages/home/home-page.tsx
+++ superhero-directory/src/pages/home/home-page.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { superheroApi } from '~entities/superhero';
 
@@ -8,10 +9,9 @@ export function HomePage() {
   const { data, isLoading, error } = superheroApi.useSearchSuperheros({
     query: searchValue,
   });
-  console.log(data, isLoading, error);
 
   return (
-    <>
+    <div>
       <h1 className="font-display text-center text-4xl">Superhero Directory</h1>
       <p>
         Welcome to the Superhero Directory! Here you can find information about
@@ -28,7 +28,20 @@ export function HomePage() {
           Failed to load superhero data.
         </p>
       )}
-      {data && <div>results</div>}
-    </>
+      {data?.results.length === 0 && (
+        <p className="text-center text-gray-500">No superheroes found.</p>
+      )}
+      {data?.results && data.results.length > 0 && (
+        <ul>
+          {data.results.map((superhero) => (
+            <Link key={superhero.id} to={`/${superhero.id}`}>
+              <li>
+                <p>{superhero.name}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
